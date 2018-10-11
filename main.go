fe31733e0890434ef013e57a8ee0641bdbaf5ed8
Robert Reilly <rreilly@edrnet.com>
2018-10-11 16:25:45 -0400

    testing

--- main.go
+++ main.go
@@ -1,18 +1,47 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-const webContent = "Hello World!"
+const (
+	version = "v1.0.0"
+)
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	log.Fatal(http.ListenAndServe(":80", nil))
-}
+	log.Println("Starting helloworld application...")
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, webContent)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello universe!\n")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, version)
+	})
+
+	s := http.Server{Addr: ":80"}
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	log.Println("Shutdown signal received, exiting...")
+
+	s.Shutdown(context.Background())
 }
